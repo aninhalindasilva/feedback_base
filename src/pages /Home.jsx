93b0c46910b66e30ad920c0ff60c4200904486ab
aninhalindasilva anaclara <anne.silvajmd@gmail.com>
2024-06-09 21:56:21 +0000

    estilização completa

--- src/pages /Home.jsx
+++ src/pages /Home.jsx
@@ -1,27 +1,44 @@
-import { ButtonContainer, CardContainer, IconContainer } from "./Home.styled";
+import { ButtonContainer, CardContainer, IconContainer, ResulContainer } from "./Home.styled";
 
 import IconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export function Home() {
+  let aparecerResultado = true
+
   return (
-    <CardContainer>
-      <IconContainer>
-        <img src={iconStar.svg} alt="Icone de estrela" />
-      </IconContainer>
+     aparecerResultado === false ? (
+      <CardContainer>
+        <IconContainer>
+          <img src={iconStar.svg} alt="Icone de estrela" />
+        </IconContainer>
 
-      <h1>Como foi o atendimento?</h1>
-      <p> Conte-nos como foi o nossa ajuda com sua solicitação. Agradecemos muito seu fedback para podemos melhorar nosso atendimento!</p>
-    
-      <ButtonContainer>
-       <button>1</button>
-       <button>2</button>
-       <button>3</button>
-       <button>4</button>
-       <button>5</button>
-      </ButtonContainer>
-    
-      <button>Enviar</button>
-     </CardContainer>
+        <h1>Como foi o atendimento?</h1>
+        <p> Conte-nos como foi o nossa ajuda com sua solicitação. Agradecemos muito seu fedback para podemos melhorar nosso atendimento!</p>
+      
+        <ButtonContainer>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        </ButtonContainer>
+      
+        <button>Enviar</button>
+      </CardContainer>
+    ): (
+      <CardContainer>
+        <img src={thankYouImg} alt="img de agradecimento"/>
+        
+        <ResulContainer>
+          <p> Você selecionou 4 de 5</p>
+          
+        </ResulContainer>
+        
+        <h1>Obrigado!</h1>
 
+        <p>Agradecemos por dedicar seu tempo para analisar. Se precisar de mais suporte, não hesite em contato </p>
+        </CardContainer>
+    )
   )
 }
--- src/pages /Home.styled.js
+++ src/pages /Home.styled.js
@@ -9,7 +9,7 @@ export const CardContainer = style.div`
 
     h1{
         font-size:2.8rem;
-        color: ${propons => props.theme.white};
+        color: ${props => props.theme.white};
         margin-button:1.5rem;
         text-aligs:center;
     }
@@ -17,10 +17,92 @@ export const CardContainer = style.div`
     p{
         font-size:1.5rem;
         line-height:2.4rem;
-        color: ${propons => props.theme.textColor};
+        color: ${props => props.theme.textColor};
         text-aligs:center;
     }
-`
-export const IconContainer = style.div``
 
-export const ButtonContainer = style.div``
+    $ > button{
+        width: 100%
+        padding-block: 1.1rem;
+        border-radius:2.25rem;
+        border:none;
+        font-size:1.5rem;
+        font-weight:700;
+        letter-spacing:2px;
+        text-transform:uppercasse;
+        background ${props => props.theme.submitButtonColor};
+        color: ${props => props.theme.white};
+        cursor:pointer;
+    }
+
+    &:hover{
+    background ${props => props.theme.white};
+    color: ${props => props.theme.submitButtonColor};
+    transition: all .3s;
+    }
+
+    & > img{
+        display: block;
+        margin: 1.3 auto 3.2rem;
+    }
+
+`
+export const IconContainer = style.div`
+    background ${propons => props.theme.feedBackButtonColor};
+    width: 4.8rem;
+    height: 4.8rem;
+    border-radius:50%;
+
+    display:flex;
+    justify-content:center;
+    aling-items:center;
+
+    magin-botton:3rem;
+`
+
+export const ButtonContainer = style.div`
+    margin-top:2.4rem;
+    display:flex;
+    justify-content:space-between;
+
+    button{
+        width: 5.1rem;
+        height: 5.1rem;
+        border-radius:50%;
+        border:none;
+        background ${props => props.theme.feedBackButtonColor};
+        color: ${props => props.theme.textColor};
+        font-wight: 700;
+        line-height: 2.4rem;
+        cursor: pointer;
+        transition: all.4s;
+
+        &:focus{
+            color: ${props => props.theme.white};
+            background ${props => props.theme.feedBackButtonText};
+            }
+
+        &:hover{
+            color: ${props => props.theme.white};
+            background ${props => props.theme.submitButtonColor};
+        
+        }
+    }
+
+
+`
+
+export const ResulContainer= style.p`
+    padding: 0.4rem 1.5rem;
+    background ${props => props.theme.feedBackButtonText};
+    width: fit-content;
+    margin-inline: auto;
+    margin-bottom: 3.2rem;
+    border-radius:2.25rem;
+
+    p{
+    color: ${props => props.theme.white};
+
+    
+    }
+`
