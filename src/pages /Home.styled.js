839390a521f6c35cb3f85cd6a402c0b1eeea5ae8
aninhalindasilva anaclara <anne.silvajmd@gmail.com>
2024-06-09 19:40:58 +0000

    estilização do h1 e p do card

--- src/pages /Home.styled.js
+++ src/pages /Home.styled.js
@@ -4,6 +4,22 @@ export const CardContainer = style.div`
     background: ${propons => props.theme.cardGradiend};
     max-width: 42.2rem;
     padding:3.2rem;
+    border-radius:3rem;
+    
+
+    h1{
+        font-size:2.8rem;
+        color: ${propons => props.theme.white};
+        margin-button:1.5rem;
+        text-aligs:center;
+    }
+
+    p{
+        font-size:1.5rem;
+        line-height:2.4rem;
+        color: ${propons => props.theme.textColor};
+        text-aligs:center;
+    }
 `
 export const IconContainer = style.div``
 
