0a5ab5acd702db35581756cc5ff5367070bb067f
aninhalindasilva anaclara <anne.silvajmd@gmail.com>
2024-06-09 16:07:26 +0000

    instalação e inplantação

---
+++ src/pages /App.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { Home } from "./pages/Home"
+import { theme } from "./style/theme.js"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
+
+export function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  ) 
+}
---
+++ src/style/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    homeBackground: "#131518",
+    cardGradient: "radial-dradient(98.96% 98.96% at 50% 0%, #232A34 0% #181E27 %100)",
+    textColor: "#9697AD",
+    submitButtonColor: "#FC7614",
+    white: "#FFF",
+    feedBackButtonColor:"#262E38",
+    feedBackButtonText:"#7C8798"
+}
